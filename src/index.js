acfd31c36a6ba7485826bb8d46c74ad444975d9f
konstantin.korovin <cosmo.stan@yandex.com>
2021-01-12 22:04:56 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-export default (a, b, op) => {
-  switch (op) {
-    case '+':
-      return a + b;
-    case '-':
-      return a - b;
-    case '*':
-      return a * b;
-    default:
-      return NaN;
-  }
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
+
+export const greeting = () => {
+  console.log(`Hello, ${name}!`);
+};
+
+export const checkAnswer = (game) => {
+  const iter = (counter) => {
+    const result = game();
+
+    if (result.userAnswer === result.correctAnswer && counter < 3) {
+      console.log('Correct!');
+      return iter(counter + 1, game);
+    }
+    if (result.userAnswer !== result.correctAnswer) {
+      return console.log(`'${result.userAnswer}' is wrong answer :(. Correct answer was '${result.correctAnswer}'.\nLet's try again, ${name}!`);
+    }
+
+    return console.log(`Correct!\nCongratulations, ${name}!`);
+  };
+
+  return iter(1);
 };
