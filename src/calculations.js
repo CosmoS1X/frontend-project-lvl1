ae31203b469e0a1f80a1ff8322604ac1ca47953e
konstantin.korovin <cosmo.stan@yandex.com>
2021-01-12 22:06:20 +0300

    add calculations.js

---
+++ src/calculations.js
@@ -0,0 +1,27 @@
+const randomInt = (from, to) => {
+  const min = Math.ceil(from);
+  const max = Math.floor(to);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+export const getRandomInt = () => randomInt(1, 100);
+
+export const getRandomOp = () => {
+  const operators = ['+', '-', '*'];
+  const rndElement = Math.floor(Math.random() * operators.length);
+
+  return operators[rndElement];
+};
+
+export const calculate = (a, b, op) => {
+  switch (op) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    default:
+      return NaN;
+  }
+};
